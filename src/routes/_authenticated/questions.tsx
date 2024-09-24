65d69e60b5560d582f7101dd963e0e455a552aa3
NganTrucLe <ngantruc2003@gmail.com>
2024-09-24 21:03:36 +0700

    feat(lap-208): add question

--- src/routes/_authenticated/questions.tsx
+++
@@ -1,6 +0,0 @@
-import QuestionListPage from "@/components/pages/questions";
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/questions")({
-  component: () => <QuestionListPage />,
-});
